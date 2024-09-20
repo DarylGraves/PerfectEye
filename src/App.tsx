fac7a46e9a9d24daca7416943b516310d4e36e08
Daryl Graves <d.graves88@outlook.com>
2024-09-20 17:21:28 +0100

    Initial Template.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import "./App.css";
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import {
+  createTheme,
+  MantineProvider,
+  AppShell,
+  AppShellHeader,
+} from "@mantine/core";
+import Navbar from "./Navbar";
+
+const theme = createTheme({
+  fontFamily: "Montserrat, sans-serif",
+  defaultRadius: "md",
+});
 
 function App() {
   return (
-    <MantineProvider>
-      <div>Hello</div>
+    <MantineProvider theme={theme}>
+      <AppShell>
+        <AppShellHeader>
+          <div>TestHeader</div>
+        </AppShellHeader>
+        <AppShell.Navbar>
+          <Navbar></Navbar>
+          <div>TestNavbar</div>
+        </AppShell.Navbar>
+        <div>Hello, World!</div>
+      </AppShell>
     </MantineProvider>
   );
 }
