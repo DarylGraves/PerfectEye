d93814ed707ff971c06a2a22936d9bf540b451b6
Daryl Graves <46203429+DarylGraves@users.noreply.github.com>
2024-10-16 21:53:24 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/PerfectEye/',
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'], // Ensure these extensions are resolved
   },
-})
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js', 'jsx'],   // For JavaScript and JSX files
+      'text/typescript': ['ts', 'tsx'],          // For TypeScript and TSX files
+    },
+  },
+});
