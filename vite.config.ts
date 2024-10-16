d7a062b938798fe2b699dfa165ca91a518155841
Daryl Graves <46203429+DarylGraves@users.noreply.github.com>
2024-10-16 21:57:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,9 @@ export default defineConfig({
     extensions: ['.js', '.jsx', '.ts', '.tsx'], // Ensure these extensions are resolved
   },
   server: {
-    middlewareMode: true,  // Use middleware mode for customizing responses
-    setupMiddleware(app) {
-      // Custom middleware for MIME type handling
-      app.use((req: { url: string }, res: { setHeader: (arg0: string, arg1: string) => void }, next: () => void) => {
+    configureServer(server) {
+      server.middlewares.use((req: any, res: any, next: any) => {
+        // Custom MIME type handling for .js and .tsx files
         if (req.url.endsWith('.js')) {
           res.setHeader('Content-Type', 'application/javascript');
         } else if (req.url.endsWith('.tsx')) {
