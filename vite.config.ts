79269533182d61c2824d3064aecc464d27d0fd1d
Daryl Graves <46203429+DarylGraves@users.noreply.github.com>
2024-10-16 21:59:42 +0100

    Update vite.config.ts
    Reverting back.

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/PerfectEye/',
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'], // Ensure these extensions are resolved
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
-  server: {
-    configureServer(server) {
-      server.middlewares.use((req: any, res: any, next: any) => {
-        // Custom MIME type handling for .js and .tsx files
-        if (req.url.endsWith('.js')) {
-          res.setHeader('Content-Type', 'application/javascript');
-        } else if (req.url.endsWith('.tsx')) {
-          res.setHeader('Content-Type', 'text/typescript');
-        }
-        next();
-      });
-    },
-  },
-});
+})
