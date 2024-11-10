34587d247585ea92f2fdb8afb2b5b2f3276ab0f4
Daryl Graves <d.graves88@outlook.com>
2024-11-10 22:25:17 +0000

    Adding CounterStrike.

--- src/Render.tsx
+++ src/Render.tsx
@@ -2,9 +2,17 @@ import Scene from "./Scene";
 import N64_GEPD from "./renders/N64_GEPD";
 import UnrealTournament from "./renders/UnrealTournament";
 import Doom from "./renders/Doom";
+import ThreeJs from "./renders/ThreeJs";
+import CounterStrike from "./renders/CounterStrike";
 
 export default function Render({ setupScene }: { setupScene: Scene }) {
   switch (setupScene.renderer) {
+    case "CounterStrike":
+      return <CounterStrike scene={setupScene} />;
+      break;
+    case "ThreeJs":
+      return <ThreeJs scene={setupScene} />;
+      break;
     case "N64_GEPD":
       return <N64_GEPD scene={setupScene} />;
       break;
